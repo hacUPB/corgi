9e0304046017361eda575fbb98e51323faaa7015
Cheira Marulanda López <cheira.marulanda@upb.edu.co>
2023-03-03 07:47:20 -0500

    contador llaves

---
+++ corgi_202301-master/Assets/Juego/Scripts/ControlLlaves.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ControlLlaves : MonoBehaviour
+{
+    [SerializeField]
+    private TMP_Text txt_contador_llaves;
+    private int contador_llaves;
+
+    public void ActualizarPuntaje(int valor)
+    {
+        contador_llaves += valor;
+        ActualizarValorUI();
+    }
+    public void ActualizarValorUI()
+    {
+        txt_contador_llaves.text ="" + contador_llaves;
+    }
+}
--- corgi_202301-master/Assets/Juego/Scripts/Llave.cs
+++ corgi_202301-master/Assets/Juego/Scripts/Llave.cs
@@ -6,9 +6,21 @@ using MoreMountains.CorgiEngine;
 
 public class Llave : PickableItem
 {
+    [SerializeField]
+    private GameObject ui_puntaje;
+    [SerializeField]
+    private int valor_llave;
+    private ControlLlaves control_llaves;
+
+    public void Start()
+    {
+        //PickableItem.Start();
+        control_llaves = ui_puntaje.GetComponent<ControlLlaves>();
+    }
     protected override void Pick(GameObject picker)
     {
 
-        Debug.Log("Recolección");
+        //Debug.Log("Recolección");
+        control_llaves.ActualizarPuntaje(valor_llave);
     }
 }
