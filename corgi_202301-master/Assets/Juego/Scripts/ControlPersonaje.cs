4bd8ebc135a1933098c1c7a298e7c3ff2d396f65
Cheira Marulanda López <cheira.marulanda@upb.edu.co>
2023-03-10 07:53:44 -0500

    Pasar de nivel

---
+++ corgi_202301-master/Assets/Juego/Scripts/ControlPersonaje.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MoreMountains.CorgiEngine;
+
+public class ControlPersonaje : Character
+{
+    public override void Reset()
+    {
+        Debug.Log("test");
+    }
+}
---
+++ corgi_202301-master/Assets/Juego/Scripts/ControlPuntaje.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlPuntaje : MonoBehaviour
+{
+
+    public static ControlPuntaje Instance { get; private set; }
+}
