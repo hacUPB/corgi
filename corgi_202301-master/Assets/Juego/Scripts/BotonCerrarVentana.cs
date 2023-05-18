0d891a7405ddfbdbf0332a84276dd62d9f05db0d
Nivekke <sofia.henao@upb.edu.co>
2023-05-18 10:56:48 -0500

    Agregar personajes y tiempo

--- corgi_202301-master/Assets/Juego/Scripts/BotonCerrarVentana.cs
+++ corgi_202301-master/Assets/Juego/Scripts/BotonCerrarVentana.cs
@@ -4,37 +4,19 @@ using UnityEngine;
 using MoreMountains.CorgiEngine;
 using MoreMountains.Tools;
 
-public class BotonCerrarVentana : MonoBehaviour, MMEventListener<MMDamageTakenEvent>
+public class BotonCerrarVentana : MonoBehaviour
 {
     [SerializeField]
     private GameObject ventana;
-    private int tiempo;
-    public int Tiempo { get => tiempo; set => tiempo = value; }
+    
 
     public void OnCerrarVentana()
     {
         ventana.SetActive(false);
         GameManager.Instance.UnPause();
-        Tiempo = 65;
+        MMEventManager.TriggerEvent(new MMGameEvent("facil"));
+      
 
-    }
-    public virtual void OnMMEvent(MMDamageTakenEvent easyevent)
-    {
-        // here we start a coroutine that will display our achievement
-        if (Tiempo==65)
-        {
-            MMEventManager.TriggerEvent(new MMGameEvent("facil"));
-            Tiempo = 65;
-        }
-
-    }
-    void OnEnable()
-    {
-        this.MMEventStartListening<MMDamageTakenEvent>();
-    }
-    void OnDisable()
-    {
-        this.MMEventStopListening<MMDamageTakenEvent>();
     }
 
 }
